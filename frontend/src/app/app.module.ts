05e2b71ec07899a3ea83b7c11a1f569182bd5c2e
Guilherme <guilherme.viegas@tecnico.ulisboa.pt>
2020-05-17 17:08:12 +0100

    Changed menu to navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UpperMainMenuComponent } from './upper-main-menu/upper-main-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { UpperMainMenuComponent } from './upper-main-menu/upper-main-menu.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
