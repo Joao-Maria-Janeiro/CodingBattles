806d3d9b232c8b3f3fa76f93543772e5ba799f33
Joao-Maria-Janeiro <joaomariajaneiro1@gmail.com>
2020-05-26 13:18:41 +0100

    Updated navbar details

--- frontend/src/app/upper-main-menu/upper-main-menu.component.ts
+++ frontend/src/app/upper-main-menu/upper-main-menu.component.ts
@@ -22,6 +22,10 @@ export class UpperMainMenuComponent implements OnInit {
   ngOnInit() {
   this.sharedService.currentUsername.subscribe(username => this.username = username);
     this.sharedService.currentLog.subscribe(isLoggedIn => this.islogged = isLoggedIn);
+    window.localStorage.getItem('user_token') == null ? (this.islogged = false) : (this.islogged = true);
+    if(this.islogged) {
+      this.username = window.localStorage.getItem('username');
+    }
   }
 
   /*isLogged() : boolean {
